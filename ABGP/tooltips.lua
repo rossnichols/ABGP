15b2ef51188a2843948830a1fad912206b33b081
Ross Nichols <rnichols@microsoft.com>
2020-04-07 19:49:33 -0700

    Add limit of 3 to the tooltip

--- ABGP/tooltips.lua
+++ ABGP/tooltips.lua
@@ -20,15 +20,33 @@ function ABGP:HookTooltips()
                 end
 
                 if IsAltKeyDown() then
-                    local printedHeader = false;
+                    local limit = 3;
                     local gpHistory = _G.ABGP_Data[value.phase].gpHistory;
+
+                    -- First pass: count
+                    local count = 0;
                     for _, data in ipairs(gpHistory) do
                         if data.item == itemName then
-                            if not printedHeader then
-                                printedHeader = true;
-                                self:AddLine("Item History:", c[1], c[2], c[3]);
+                            count = count + 1;
+                        end
+                    end
+
+                    if count > 0 then
+                        local extra = "";
+                        if count > limit then
+                            extra = (" (%d of %d)"):format(limit, count);
+                        end
+                        self:AddLine(("Item History%s:"):format(extra), c[1], c[2], c[3]);
+                        count = 0;
+
+                        for _, data in ipairs(gpHistory) do
+                            if data.item == itemName then
+                                count = count + 1;
+                                if count > limit then
+                                    break;
+                                end
+                                self:AddDoubleLine(ABGP:ColorizeName(data.player), data.date, 1, 1, 1, 1, 1, 1);
                             end
-                            self:AddDoubleLine(ABGP:ColorizeName(data.player), data.date, 1, 1, 1, 1, 1, 1);
                         end
                     end
                 end
