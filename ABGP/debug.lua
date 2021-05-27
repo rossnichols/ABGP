93f21a8f53a9e85abb8ef80c348b3573e83ef6cb
Ross Nichols <ross.nichols@live.com>
2021-05-26 17:20:15 -0700

    Update debug version

--- ABGP/debug.lua
+++ ABGP/debug.lua
@@ -38,5 +38,5 @@ function ABGP:SetDebug(enable)
     self:Set("debug", enable);
 end
 
-ABGP.VersionOverride = "8.0.5";
-ABGP.VersionCmpOverride = "8.0.5";
+ABGP.VersionOverride = "8.0.6";
+ABGP.VersionCmpOverride = "8.0.6";
